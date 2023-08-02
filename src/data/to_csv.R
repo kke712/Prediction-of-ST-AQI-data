1f1e86cd5f5b83c250d83bf3d0df9a90c66aa904
Kyeongwon Lee <kwlee1718@gmail.com>
2023-08-02 23:02:09 +0900

    update to_csv.R

--- src/data/to_csv.R
+++ src/data/to_csv.R
@@ -2,7 +2,7 @@ train_list <- readRDS("./data/train_list_norm.rds")
 test_list <- readRDS("./data/test_list_norm.rds")
 
 # to csv
-for (i in seq_len(length(testind))) {
+for (i in seq_len(length(test_list))) {
     write.csv(train_list[[i]], paste0("./data/csv/train_", i, ".csv"))
     write.csv(test_list[[i]], paste0("./data/csv/test_", i, ".csv"))
 }
